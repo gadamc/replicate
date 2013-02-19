7b66f3a132e54009c3c9db35b1cf2fd309f35c73
gadamc <adam.cox@kit.edu>
2013-02-20 00:29:41 +0100

    new initial commit.... removing all old commits since they had login creds

---
+++ replicateAll.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from couchdbkit import Server, Database
+from datetime import datetime
+import json
+import strjson
+import sys
+
+try:
+    creds = strjson.load('localcredentials.json')
+    print json.dumps(creds, indent=1)
+
+except Exception as e:
+    print e
+    print 'failed to open credentials file'
+    sys.exit(1)
+    
+s = Server(creds['server'])
+
+db = s['_replicator']
+vr = db.all_docs()
+
+print "UTC Time:",  datetime.utcnow()
+for row in vr:
+
+    try:
+        doc = db[row['id']]
+        if doc['_replication_state'] == 'error':
+            print 'restarting', row['id']
+            del doc['_replication_state']
+            db.save_doc(doc)
+        
+    except:
+      pass
+
